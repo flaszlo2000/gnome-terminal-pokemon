951b2cb883076f939a3ba6b0f89051b2f8650c42
flaszlo2000 <f.laszlo2000@gmail.com>
2025-04-03 05:46:38 +0200

    updated python  path, removed trailing newline from colorscript with bash
    removed trailing newline that created one unnecessary line at the end of the art.
    fixed python script path to make installer not fail (but atm in a hardcoded way)

--- pokemon_show_and_wait.sh
+++ pokemon_show_and_wait.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -n "$2" ]; then
-    pokemon-colorscripts -s -n $1 && python3 -c "input()"
+    pokemon-colorscripts -s -n $1 | head -c -1 && python3 -c "input()"
 else
-    pokemon-colorscripts --no-title -n $1 && python3 -c "input()"
+    pokemon-colorscripts --no-title -n $1 | head -c -1 && python3 -c "input()"
 fi
